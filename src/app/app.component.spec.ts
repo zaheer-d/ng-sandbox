52ffb8bab3d71758ba362b0f91c380397570ed8a
Zaheer <zaheer.dollie@ipreo.com>
2018-02-18 21:03:49 +0200

    fixed 2 failing test. all tests passing :)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,65 +1,75 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {SomeThingModule, TestModel} from './some-thing/some-thing.module';
+import { SomeThingModule, TestModel } from './some-thing/some-thing.module';
 
 describe('AppComponent', () => {
+    let somethingModuleMock;
 
-  let somethingModuleMock;
+    // {provide: SomeThingModule,
+    //   useValue: class {
+    //   thisIsMyNewMethod = jasmine.createSpy('thisIsMyNewMethod');
+    // }
+    beforeEach(
+        async(() => {
+            TestBed.configureTestingModule({
+                providers: [SomeThingModule],
+                declarations: [AppComponent]
+            }).compileComponents();
+        })
+    );
 
-  // {provide: SomeThingModule,
-  //   useValue: class {
-  //   thisIsMyNewMethod = jasmine.createSpy('thisIsMyNewMethod');
-  // }
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [SomeThingModule],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    it(
+        'should create the app',
+        async(() => {
+            const fixture = TestBed.createComponent(AppComponent);
+            const app = fixture.debugElement.componentInstance;
+            expect(app).toBeTruthy();
+        })
+    );
 
-  }));
+    it(
+        `should have as title 'app'`,
+        async(() => {
+            const fixture = TestBed.createComponent(AppComponent);
+            const app = fixture.debugElement.componentInstance;
+            expect(app.title).toEqual('app');
+        })
+    );
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    it(
+        'should render title in a h1 tag',
+        async(() => {
+            const fixture = TestBed.createComponent(AppComponent);
+            fixture.detectChanges();
+            const compiled = fixture.debugElement.nativeElement;
+            expect(compiled.querySelector('h1').textContent).toContain(
+                'Welcome to app!'
+            );
+        })
+    );
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+    describe('SomethingModule works', () => {
+        beforeEach(() => {
+            somethingModuleMock = TestBed.get(SomeThingModule);
+        });
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+        it('should get data package from myTestObs', async() => {
+            const mySub$ = somethingModuleMock.myTestObservable;
+            console.log('myTestObs', somethingModuleMock);
+            mySub$.subscribe((d: TestModel) => {
+                expect(d.name).toEqual('hello');
+            });
+            somethingModuleMock.thisIsMyNewMethod('somewords');
+        });
 
-  describe('SomethingModule works', () => {
-    beforeEach(() => {
-      somethingModuleMock = TestBed.get(SomeThingModule);
+        it('should return whats up module', () => {
+            let result;
+            spyOn(somethingModuleMock, 'thisIsMyNewMethod').and.callFake(p => {
+                console.log(p);
+                return p;
+            });
+            result = somethingModuleMock.thisIsMyNewMethod('yyuyuu');
+            expect(result).not.toContain('hello');
+        });
     });
-
-    it('should get data package from myTestObs', () => {
-      const mySub$ = somethingModuleMock.myTestObservable;
-      mySub$.subscribe((d: TestModel) => {
-        expect(d.name).toEqual('hello');
-      });
-      somethingModuleMock.thisIsMyNewMethod('somewords');
-    });
-
-    it('should return whats up module', () => {
-      let result;
-      spyOn(somethingModuleMock, 'thisIsMyNewMethod').and.callFake((p) => {
-        console.log(p);
-        return p;
-      });
-      result = somethingModuleMock.thisIsMyNewMethod('yyuyuu');
-      expect(result).not.toContain('hello');
-    });
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import {SomeThingModule, TestModel} from './some-thing/some-thing.module';
+import { SomeThingModule, TestModel } from './some-thing/some-thing.module';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  title = 'app';
-  myModule;
-  textFromModule = '';
-  messageText = '';
-
-  constructor(myModule: SomeThingModule) {
-    this.myModule = myModule;
-  }
-
-  ngOnInit(): void {
-    this.textFromModule = this.myModule.thisIsMyNewMethod('words of wisdom');
-
-    this.myModule.myTestObservable.subscribe((data: TestModel) => {
-      this.messageText = data.name;
-    });
-
-  }
+    title = 'app';
+    myModule;
+    textFromModule = '';
+    messageText = '';
 
 
 
 
+    
 
+    constructor(myModule: SomeThingModule) {
+        this.myModule = myModule;
+    }
 
+    ngOnInit(): void {
+        this.textFromModule = this.myModule.thisIsMyNewMethod(
+            'words of wisdom'
+        );
+
+        this.myModule.myTestObservable.subscribe((data: TestModel) => {
+            this.messageText = data.name;
+        });
+    }
 }
